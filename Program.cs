432640d7ad4744e359a98f3ba50b1acf06e0413d
Rafid <rafidn@outlook.com>
2025-05-21 18:46:25 -0500

    Lesson 50: Async Await Tasks

--- Program.cs
+++ Program.cs
@@ -1,129 +1,41 @@
-﻿using System;
-using System.Data;
-using System.Globalization;
-using System.Text.Json;
-using AutoMapper;
-using Dapper;
-using HelloWorld.Data;
-using HelloWorld.Models;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore.Storage.Json;
-using Microsoft.Extensions.Configuration;
-
-namespace HelloWorld
+﻿namespace HelloWorld
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            IConfiguration config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json")
-                .Build();
-            DataContextDapper dapper = new DataContextDapper(config);
-
-            string computersJson = File.ReadAllText("computersSnake.json");
-
-            Mapper mapper = new Mapper(new MapperConfiguration((cfg) =>
+            Task firstTask = new Task(() =>
             {
-                cfg.CreateMap<ComputerSnake, Computer>()
-                    .ForMember(destination =>
-                        destination.ComputerId, options =>
-                            options.MapFrom(source =>
-                                source.computer_id))
-                    .ForMember(destination =>
-                        destination.Motherboard, options =>
-                            options.MapFrom(source =>
-                                source.motherboard))
-                    .ForMember(destination =>
-                        destination.CPUCores, options =>
-                            options.MapFrom(source =>
-                                source.cpu_cores))
-                    .ForMember(destination =>
-                        destination.HasWifi, options =>
-                            options.MapFrom(source =>
-                                source.has_wifi))
-                    .ForMember(destination =>
-                        destination.HasLTE, options =>
-                            options.MapFrom(source =>
-                                source.has_lte))
-                    .ForMember(destination =>
-                        destination.ReleaseDate, options =>
-                            options.MapFrom(source =>
-                                source.release_date))
-                    .ForMember(destination =>
-                        destination.Price, options =>
-                            options.MapFrom(source =>
-                                source.price))
-                    .ForMember(destination =>
-                        destination.VideoCard, options =>
-                            options.MapFrom(source =>
-                                source.video_card));
-            }));
+                Thread.Sleep(100);
+                Console.WriteLine("Task 1");
+            });
+            firstTask.Start();
 
+            Task secondTask = ConsoleAfterDelayAsync("Task 2", 150);
 
-            // Console.WriteLine(computersJson);
+            ConsoleAfterDelay("Delay", 50);
 
+            Task thirdTask = ConsoleAfterDelayAsync("Task 3", 50);
 
-            // // NEW VERSION OF JSON
-            // JsonSerializerOptions options = new JsonSerializerOptions()
-            // {
-            //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            // };
+            await secondTask;
 
-
-            //  USING MAPPING
-            IEnumerable<ComputerSnake>? computerSystemMapped = JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computersJson);
-
-            if (computerSystemMapped != null)
-            {
-                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computerSystemMapped);
-                Console.WriteLine("Automapper Count: " + computerResult.Count());
-                // foreach (Computer computer in computerResult)
-                // {
-                //     Console.WriteLine(computer.Motherboard);
-                // }
-            }
-
-            // USING JSON PROPERTYNAME
-            IEnumerable<Computer>? computerSystem = JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson);
-            Console.WriteLine("JSON Property Count: " + computerSystem.Count()); 
-            // foreach (Computer computer in computerSystem)
-            // {
-            //     Console.WriteLine(computer.Motherboard);
-            // }
-
-            // if (computers != null)
-            // {
-            //     foreach (Computer computer in computers)
-            //     {
-            //         string sql = @"INSERT INTO TutorialAppSchema.Computer (
-            //             Motherboard,
-            //             HasWifi,
-            //             HasLTE,
-            //             ReleaseDate,
-            //             Price,
-            //             VideoCard
-            //         ) VALUES ('" + EscapeSingleQuote(computer.Motherboard)
-            //                 + "','" + computer.HasWifi
-            //                 + "','" + computer.HasLTE
-            //                 + "','" + (computer.ReleaseDate != null ? computer.ReleaseDate.Value.ToString("yyyy-MM-dd") : "")
-            //                 + "','" + computer.Price.ToString("0.00", CultureInfo.InvariantCulture)
-            //                 + "','" + EscapeSingleQuote(computer.VideoCard)
-            //         + "')\n";
-            //         dapper.ExecuteSql(sql);
-            //     }
-            // }
-
-            // string computersCopySystem = JsonSerializer.Serialize(computers, options);
-
-            // File.WriteAllText("computersCopySystem.txt", computersCopySystem);
-
+            await firstTask;
+            Console.WriteLine("After the Task was created");
+            await thirdTask;
         }
 
-        static string EscapeSingleQuote(string input)
+
+
+        static void ConsoleAfterDelay(string text, int delayTime)
         {
-            string output = input.Replace("'", "''");
-            return output;
+            Thread.Sleep(delayTime);
+            Console.WriteLine(text);
+        }
+        
+        static async Task ConsoleAfterDelayAsync(string text, int delayTime)
+        {
+            await Task.Delay(delayTime);
+            Console.WriteLine(text);
         }
 
     }
--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71707237c51b8a24710053c8f94598c393d15ce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c58fba6a1b031fbcfb2408eb6d39517af240e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
